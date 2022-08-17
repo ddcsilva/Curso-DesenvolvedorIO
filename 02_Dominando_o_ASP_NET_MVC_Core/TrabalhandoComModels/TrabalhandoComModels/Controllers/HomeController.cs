edc52fdf840e59c89ba39a004b5add9cf2279d82
Danilo <danilo.silva@msn.com>
2022-08-17 18:23:43 -0300

    Aula - Trabalhando com Models, Validação e DataAnnotations

---
+++ 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComModels/TrabalhandoComModels/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TrabalhandoComModels.Models;
+
+namespace TrabalhandoComModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var filme = new Filme
+            {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 20000
+            };
+
+            return RedirectToAction("Privacy", filme);
+        }
+
+        public IActionResult Privacy(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComModels/TrabalhandoComModels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrabalhandoComModels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
