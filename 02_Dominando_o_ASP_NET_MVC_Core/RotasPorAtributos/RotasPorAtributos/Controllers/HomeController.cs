94da5a648e9e3f4f2f6eff323006d4d98550695e
Danilo <danilo.silva@msn.com>
2022-08-17 11:32:01 -0300

    Aula - Implementando Rotas Inteligentes

--- 02_Dominando_o_ASP_NET_MVC_Core/RotasPorAtributos/RotasPorAtributos/Controllers/HomeController.cs
+++ 02_Dominando_o_ASP_NET_MVC_Core/RotasPorAtributos/RotasPorAtributos/Controllers/HomeController.cs
@@ -4,8 +4,12 @@ using System.Diagnostics;
 
 namespace RotasPorAtributos.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
+        // Roteamento: https://docs.microsoft.com/pt-br/aspnet/core/fundamentals/routing?view=aspnetcore-6.0
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -13,17 +17,23 @@ namespace RotasPorAtributos.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
