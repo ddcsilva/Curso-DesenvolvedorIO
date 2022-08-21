a00ff0fbb09df34fd3f5f5ed4437b2887a4ebb55
Danilo <danilo.silva@msn.com>
2022-08-21 13:08:02 -0300

    Aula - Configurando uma Injeção de Dependência

---
+++ 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComDI/src/DevIO.UI.Site/Data/PedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.UI.Site.Model;
+
+namespace DevIO.UI.Site.Data
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        public Pedido ObterPedido()
+        {
+            return new Pedido();
+        }
+    }
+
+    public interface IPedidoRepository
+    {
+        Pedido ObterPedido();
+    }
+}
