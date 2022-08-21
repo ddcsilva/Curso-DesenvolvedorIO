baf5744e0533112a3e0e390d360e6d370a6f6975
Danilo <danilo.silva@msn.com>
2022-08-21 13:14:33 -0300

    Aula - Injetando Dependências no MVC

--- 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComDI/src/DevIO.UI.Site/Controllers/HomeController.cs
+++ 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComDI/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        //private readonly IPedidoRepository _pedidoRepository;
+
+        //public HomeController(IPedidoRepository pedidoRepository)
+        //{
+        //    _pedidoRepository = pedidoRepository;
+        //}
+
+        public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
     }
