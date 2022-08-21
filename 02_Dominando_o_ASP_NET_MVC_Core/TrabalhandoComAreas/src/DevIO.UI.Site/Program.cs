8f1144a31410732e06419af88525021cbcae5f44
Danilo <danilo.silva@msn.com>
2022-08-21 00:38:46 -0300

    Aula - Trabalhando com Áreas

---
+++ 02_Dominando_o_ASP_NET_MVC_Core/TrabalhandoComAreas/src/DevIO.UI.Site/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
+// Tudo inicia a partir do builder
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionando suporte a mudança de convenção da rota das areas.
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
+
+// Adicionando o MVC ao container
+builder.Services.AddControllersWithViews();
+
+// Realizando o buid das configurações que resultará na App
+var app = builder.Build();
+
+// Ativando a pagina de erros caso seja ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+// Rota de área genérica
+//app.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+// Rota de áreas especializadas
+app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
+
+// Adicionando Rota padrão
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Colocando a App para rodar
+app.Run();
