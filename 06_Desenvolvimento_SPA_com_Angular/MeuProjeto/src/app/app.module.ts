49def2b07e0fad6093b5fcf5f56aade1f21d64e4
Danilo <danilo.silva@msn.com>
2022-10-15 17:49:49 -0300

    Definindo componentes de navegação

--- 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
+++ 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
