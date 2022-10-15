f799c9b57320e622e9bb2844f57e372e55b701ec
Danilo <danilo.silva@msn.com>
2022-10-15 18:34:27 -0300

    Roteamento e Configuração

--- 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
+++ 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -7,20 +9,19 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
-    SobreComponent,
-    ContatoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        HomeComponent,
+        FooterComponent,
+        SobreComponent,
+        ContatoComponent
+    ],
+    imports: [BrowserModule, [RouterModule.forRoot(rootRouterConfig, { useHash: false })]],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
