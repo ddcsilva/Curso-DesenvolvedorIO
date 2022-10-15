847513da382a4da32cb80fe296265926e40546f6
Danilo <danilo.silva@msn.com>
2022-10-15 20:31:03 -0300

    Serviços utilizando HttpClient

--- 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
+++ 06_Desenvolvimento_SPA_com_Angular/MeuProjeto/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { ProdutoService } from './produtos/produtos.service';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
 @NgModule({
     declarations: [
@@ -21,14 +23,15 @@ import { DataBindingComponent } from './demos/data-binding/data-binding.componen
         FooterComponent,
         SobreComponent,
         ContatoComponent,
-        DataBindingComponent
+        DataBindingComponent,
+        ListaProdutoComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
     ],
-    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    providers: [ProdutoService, { provide: APP_BASE_HREF, useValue: '/' }],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
