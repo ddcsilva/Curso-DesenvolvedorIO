7a13acb93215835dcd1e5c9e5acda21713be314b
Danilo <danilo.silva@msn.com>
2022-10-15 16:49:41 -0300

    Criação de Módulo e Serviço de exemplo

--- 06_Desenvolvimento_SPA_com_Angular/MinhaAppAngular/src/app/app.module.ts
+++ 06_Desenvolvimento_SPA_com_Angular/MinhaAppAngular/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NomeComponenteComponent } from './pasta/nome-componente/nome-componente.component';
 import { HomeComponent } from './home/home.component';
+import { ServicoService } from './servico.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NomeComponenteComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NomeComponenteComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ServicoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ 06_Desenvolvimento_SPA_com_Angular/MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    // Meus componentes
+  ],
+  imports: [
+    CommonModule,
+    // Meus módulos
+  ],
+  providers: [
+    // Meus serviços
+  ],
+})
+export class FuncionalidadeModule {}
