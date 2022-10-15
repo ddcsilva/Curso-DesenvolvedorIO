866a0d4ca3634826817631d14322f76599b3256f
Danilo <danilo.silva@msn.com>
2022-10-15 16:35:18 -0300

    Primeiros Componentes

---
+++ 06_Desenvolvimento_SPA_com_Angular/MinhaAppAngular/src/app/pasta/nome-componente/nome-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  templateUrl: './nome-componente.component.html',
+})
+export class NomeComponenteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
