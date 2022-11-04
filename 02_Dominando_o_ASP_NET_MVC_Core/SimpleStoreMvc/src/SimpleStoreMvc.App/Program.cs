bc71f4e4ae39bc1a13457bd2f4b98e6afa4a0a8c
Danilo Silva <danilo.silva@msn.com>
2022-11-04 17:51:57 -0300

    Mapeando as entidades para o Banco de Dados

--- 02_Dominando_o_ASP_NET_MVC_Core/SimpleStoreMvc/src/SimpleStoreMvc.App/Program.cs
+++ 02_Dominando_o_ASP_NET_MVC_Core/SimpleStoreMvc/src/SimpleStoreMvc.App/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SimpleStoreMvc.App.Data;
+using SimpleStoreMvc.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<SimpleStoreContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
